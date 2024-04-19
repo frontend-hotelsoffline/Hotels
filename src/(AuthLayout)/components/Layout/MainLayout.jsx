60aab9e0ce26e2e5ff6a06a0dbafa643d14415db
FrancisBaah <baahfrancis88@gmail.com>
2024-04-19 17:25:37 +0400

    --

--- src/(AuthLayout)/components/Layout/MainLayout.jsx
+++ src/(AuthLayout)/components/Layout/MainLayout.jsx
@@ -3,13 +3,18 @@ import { Button, Layout, Modal, Select } from "antd";
 import Sidebar from "./Sidebar";
 import { LuBellDot } from "react-icons/lu";
 import HeaderTitle from "./HeaderTitle";
+import { HiBars4 } from "react-icons/hi2";
+import { useState } from "react";
 const { Header, Content } = Layout;
 
 const MainLayout = ({ children, title }) => {
+  const [closeSidebar, setCloseSidebar] = useState(false);
   return (
     <Layout className="w-full overflow-auto flex flex-row">
-      <div className="w-[200px] border-r-[1px] border-gray-200">
-        <Sidebar />
+      <div className="max-w-[200px] border-r-[1px] border-gray-200">
+        <div className={`${closeSidebar && "hidden"}`}>
+          <Sidebar />
+        </div>
       </div>
       <div className="w-full">
         <Header
@@ -19,10 +24,14 @@ const MainLayout = ({ children, title }) => {
           }}
           className="w-full p-0 md:px-5 shadow-sm flex justify-between items-center"
         >
-          <HeaderTitle />
-          <Select
-            placeholder={<LuBellDot className="h-[17px] w-[15px]" />}
-          />
+          <div className="flex items-center gap-3">
+            <HiBars4
+              onClick={() => setCloseSidebar(!closeSidebar)}
+              className="w-7 h-7 cursor-pointer"
+            />{" "}
+            <HeaderTitle />
+          </div>
+          <Select placeholder={<LuBellDot className="h-[17px] w-[15px]" />} />
         </Header>
         <Content
           className="p-0 py-2 md:px-5 w-full"
